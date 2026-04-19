70613051af9d7f4dab42208c863eea133e73f680
Somnath Das <somnath.das@daslearning.in>
2026-04-19 13:39:28 +0530

    initial app added, need to check if battery check works on android

---
+++ app/main.py
@@ -0,0 +1,205 @@
+import os
+os.environ['KIVY_GL_BACKEND'] = 'sdl2'
+import sys
+from threading import Thread
+
+from kivymd.app import MDApp
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.navigationdrawer import MDNavigationDrawerMenu
+from kivymd.uix.menu import MDDropdownMenu
+#from kivymd.uix.filemanager import MDFileManager
+from kivymd.uix.label import MDLabel
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton, MDFloatingActionButton
+
+from kivy.clock import Clock
+from kivy.utils import platform
+from kivy.core.window import Window
+from kivy.metrics import dp, sp
+from kivy.lang import Builder
+from kivy.properties import StringProperty, NumericProperty, ObjectProperty, BooleanProperty
+
+# IMPORTANT: Set this property for keyboard behavior
+Window.softinput_mode = "below_target"
+
+## Global definitions
+__version__ = "0.0.1" # App version
+
+# Determine the base path for your application's resources
+if getattr(sys, 'frozen', False):
+    # Running as a PyInstaller bundle
+    base_path = sys._MEIPASS
+else:
+    # Running in a normal Python environment
+    base_path = os.path.dirname(os.path.abspath(__file__))
+kv_file_path = os.path.join(base_path, 'main_layout.kv')
+
+# import local screens
+from screens.setting import SettingsBox
+from screens.init_screen import ConfigInput
+from screens.control_scr import MainAppBox
+
+# imprt local APIs / platform specific modules
+if platform == "android":
+    # local APIs are managed in service
+    from jnius import autoclass
+else:
+    import psutil
+
+## define custom kivymd classes
+class ContentNavigationDrawer(MDNavigationDrawerMenu):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+class MainScreenBox(MDBoxLayout):
+    top_pad = NumericProperty(0)
+    bottom_pad = NumericProperty(0)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        if platform == "android":
+            try:
+                from android.display_cutout import get_height_of_bar
+                self.top_pad = int(get_height_of_bar('status'))
+                self.bottom_pad = int(get_height_of_bar('navigation'))
+            except Exception as e:
+                print(f"Failed android 15 padding: {e}")
+                self.top_pad = 32
+                self.bottom_pad = 48
+
+### Main App
+class AutoChargeApp(MDApp):
+    is_auto_mode_on = BooleanProperty(False)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.wake_lock = None
+
+    def build(self):
+        self.theme_cls.primary_palette = "Blue"
+        self.theme_cls.accent_palette = "Orange"
+        return Builder.load_file(kv_file_path)
+
+    def on_start(self):
+        # OS specific setups
+        if platform == "android":
+            # paths on android
+            context = autoclass('org.kivy.android.PythonActivity').mActivity
+            android_path = context.getExternalFilesDir(None).getAbsolutePath()
+            self.config_dir = os.path.join(android_path, 'config')
+            self.internal_storage = android_path
+            try:
+                Environment = autoclass("android.os.Environment")
+                self.external_storage = Environment.getExternalStorageDirectory().getAbsolutePath()
+            except Exception:
+                self.external_storage = os.path.abspath("/storage/emulated/0/")
+
+        # for other platforms (non android)
+        else:
+            self.internal_storage = os.path.expanduser("~")
+            self.external_storage = os.path.expanduser("~")
+            self.config_dir = os.path.join(self.user_data_dir, 'config')
+        os.makedirs(self.config_dir, exist_ok=True)
+        self.config_path = os.path.join(self.config_dir, 'config.json')
+        self.resp_path = os.path.join(self.config_dir, 'resp.json')
+        # the resulting elements
+        #print(self.root.ids.app_box.ids)
+        self.result_txt = self.root.ids.app_box.ids.result_text
+        Thread(target=self.battery_loop, daemon=True).start()
+
+    def get_battery_details(self):
+        battery_pct = 0
+        # OS specific setups
+        if platform == "android":
+            # get battery details
+            activity = autoclass('org.kivy.android.PythonActivity').mActivity
+            IntentFilter = autoclass('android.content.IntentFilter')
+            BatteryManager = autoclass('android.os.BatteryManager')
+            intent = activity.registerReceiver(
+                None,
+                IntentFilter('android.intent.action.BATTERY_CHANGED')
+            )
+            level = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            scale = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+            battery_pct = (level / float(scale)) * 100
+            print(f"Battery: {battery_pct:.2f}%")
+        else:
+            # Get battery details
+            battery = psutil.sensors_battery()
+            if battery:
+                battery_pct = battery.percent
+                power_plugged = battery.power_plugged
+                # Simple output
+                print(f"Battery Percentage: {battery_pct}%")
+                print(f"Power Plugged: {'Yes' if power_plugged else 'No'}")
+            else:
+                print("Battery information could not be retrieved.")
+        return battery_pct
+
+    def battery_loop(self):
+        import time
+        while True:
+            battery_pct = self.get_battery_details()
+            self.result_txt.text = f"Battery: {battery_pct}"
+            time.sleep(2)
+
+    def show_toast_msg(self, message, is_error=False, duration=3):
+        from kivymd.uix.snackbar import MDSnackbar
+        bg_color = (0.2, 0.6, 0.2, 1) if not is_error else (0.8, 0.2, 0.2, 1)
+        MDSnackbar(
+            MDLabel(
+                text = message,
+                font_style = "Subtitle1"
+            ),
+            md_bg_color=bg_color,
+            y=dp(24),
+            pos_hint={"center_x": 0.5},
+            duration=duration
+        ).open()
+
+    def show_text_dialog(self, title, text="", buttons=[]):
+        self.txt_dialog = MDDialog(
+            title=title,
+            text=text,
+            buttons=buttons
+        )
+        self.txt_dialog.open()
+
+    def txt_dialog_closer(self, instance):
+        if self.txt_dialog:
+            self.txt_dialog.dismiss()
+
+    def open_link(self, instance, url):
+        import webbrowser
+        webbrowser.open(url)
+
+    def update_link_open(self, instance):
+        self.txt_dialog_closer(instance)
+        self.open_link(instance=instance, url="https://github.com/daslearning-org/navigation-indicator/releases")
+
+    def update_checker(self, instance=None):
+        buttons = [
+            MDFlatButton(
+                text="Cancel",
+                theme_text_color="Custom",
+                text_color=self.theme_cls.primary_color,
+                on_release=self.txt_dialog_closer
+            ),
+            MDFlatButton(
+                text="Releases",
+                theme_text_color="Custom",
+                text_color="green",
+                on_release=self.update_link_open
+            ),
+        ]
+        self.show_text_dialog(
+            "Check for update",
+            f"Your version: {__version__}",
+            buttons
+        )
+
+    ## run on app exit
+    def on_stop(self):
+        print("Exiting the app...")
+
+if __name__ == '__main__':
+    AutoChargeApp().run()
